b4cc4fc54d732f859a6d4aac14df8ed3f44f8cb4
Jesus Quevedo <jesus.quevedo@morningstar.com>
2022-10-11 17:06:37 -0400

    search handler

--- src/components/products-list/ProductList.jsx
+++ src/components/products-list/ProductList.jsx
@@ -1,15 +1,37 @@
+import "./ProductList.scss";
+import { useState, useEffect } from "react";
 import { ProductItem } from "../product-item/ProductItem";
 import { Search } from "../search/Search";
-import "./ProductList.scss";
 
 export const ProductList = (props) => {
     const { products } = props;
 
-    const productsList = products.map(product => <ProductItem  key={product.id} product={product} />);
+    const [productsItems, SetProductsItems] = useState([...products]);
+
+    const searchHandler = ({target: { value}}) => {
+
+        if (value) {
+            let searchAcc = products;
+
+            const result = searchAcc.filter(({brand, model}) => {
+
+                return brand.toLowerCase().includes(value) || model.toLowerCase().includes(value);
+            });
+
+            if (result.length) {
+                SetProductsItems(result);
+            }
+
+        } else {
+            SetProductsItems(products);
+        }
+    }
+
+    const productsList = productsItems.map(product => <ProductItem  key={product.id} product={product} />);
 
     return (
         <div className="product-list">
-            <Search></Search>
+            <Search onSearch={searchHandler}></Search>
             <div className="columns is-multiline" >
                 { productsList }
             </div>
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
 import "./search.scss";
 
-export const Search = () =>{
-    const [query, updateQuery] = useState();
-
-    const onSearch = ({ target }) => {
-        const {value} = target;
-
-        updateQuery((prev) => {
-            return value;
-        });
-    };
+export const Search = (props) =>{
 
     return (
         <div className="search field">
             <p className="control has-icons-left has-icons-right">
-                <input className="input" type="text" placeholder="Search products..." value={query} onChange={onSearch}/>
+                <input className="input" type="text" placeholder="Search products..."  onChange={props.onSearch}/>
                 <span className="icon is-small is-left">
                 <i className="fas fa-search"></i>
                 </span>
